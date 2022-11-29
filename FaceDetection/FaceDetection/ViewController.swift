9a5931159887882f6c8422c78481fda24a51caf1
최민한 <chmh0411@gmail.com>
2022-11-29 17:50:35 +0900

    pod

---
+++ FaceDetection/FaceDetection/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FaceDetection
+//
+//  Created by 최민한 on 2022/11/29.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
